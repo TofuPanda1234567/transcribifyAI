63fc93fec861113a063e8f8fd8375c373e47ebb1
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2023-10-13 18:49:18 +0000

    testing

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/api/test")
+@app.route("https://shiny-adventure-g77jv9qrxxxhvqxr-3000.app.github.dev/api/test")
 def test():
     return "<p>Hello, World!</p>"
