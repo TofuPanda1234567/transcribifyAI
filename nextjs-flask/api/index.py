02acfa11e934f70e4122caccc97793dda70ddab6
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2023-10-25 19:22:36 +0000

    yeet

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -7,7 +7,7 @@ def hello_world():
 
 @app.route("/api/test/")
 def test():
-    return render_template("/nextjs-flask/app/test.html")
+    return render_template("/workspaces/transcribifyAI/nextjs-flask/app/test.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5238)
+    app.run(debug=True)
